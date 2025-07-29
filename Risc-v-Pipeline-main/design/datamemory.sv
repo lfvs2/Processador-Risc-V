5a6e7a0afbd043e4797289eef14f418fbe7434a3
Mateus Espíndola <101195071+espindoolaa@users.noreply.github.com>
2025-07-29 20:24:15 -0300

    fixing datamemory.sv

--- Risc-v-Pipeline-main/design/datamemory.sv
+++ Risc-v-Pipeline-main/design/datamemory.sv
@@ -30,7 +30,7 @@ module datamemory #(
 
   always_ff @(*) begin
     raddress = {{22{1'b0}}, a};
-    waddress = {{22{1'b0}}, {a[8:2], {2{1'b0}}}};
+    waddress = {{22{1'b0}}, a}; // byte-adressed
     Datain = wd;
     Wr = 4'b0000;
 
