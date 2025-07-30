662fb17a1fe2e8df9bc81046b8159d52ddcc5313
lfvs2 <lfvs2@cin.ufpe.br>
2025-07-30 12:49:03 -0300

    Update datamemory.sv

--- Risc-v-Pipeline-main/design/datamemory.sv
+++ Risc-v-Pipeline-main/design/datamemory.sv
@@ -28,60 +28,68 @@ module datamemory #(
       .Wr(Wr)
   );
 
-  always_ff @(*) begin
-    raddress = {{22{1'b0}}, a};
-    waddress = {{22{1'b0}}, {a[8:2], {2{1'b0}}}};
+  always_comb begin
+    raddress = {{22{1'b0}}, a[8:2], 2'b00};
+    waddress = {{22{1'b0}}, a[8:2], 2'b00};
     Datain = wd;
     Wr = 4'b0000;
-
+    rd = 32'b0;
     if (MemRead) begin
       case (Funct3)
         3'b000:	begin  //LB
         case (a[1:0])
-           	2'b00: rd <= {{24{Dataout[7]}}, Dataout[7:0]};   // Load byte 0 (bits 7:0 of Dataout)
-           	2'b01: rd <= {{24{Dataout[15]}}, Dataout[15:8]}; // Load byte 1 (bits 15:8 of Dataout)
-           	2'b10: rd <= {{24{Dataout[23]}}, Dataout[23:16]}; // Load byte 2 (bits 23:16 of Dataout)
-           	2'b11: rd <= {{24{Dataout[31]}}, Dataout[31:24]}; // Load byte 3 (bits 31:24 of Dataout)
-           	default: rd <= 'x; 
+           	2'b00: rd = {{24{Dataout[7]}}, Dataout[7:0]};   // Load byte 0 (bits 7:0 of Dataout)
+           	2'b01: rd = {{24{Dataout[15]}}, Dataout[15:8]}; // Load byte 1 (bits 15:8 of Dataout)
+           	2'b10: rd = {{24{Dataout[23]}}, Dataout[23:16]}; // Load byte 2 (bits 23:16 of Dataout)
+           	2'b11: rd = {{24{Dataout[31]}}, Dataout[31:24]}; // Load byte 3 (bits 31:24 of Dataout)
         endcase
+	end
+	3'b100:	begin  //LBU
+	case (a[1:0]) 
+		2'b00:	rd = {24'b0, Dataout[7:0]};  // Estende com 24 zeros à esquerda
+		2'b01:	rd = {24'b0, Dataout[15:8]}; // Estende com 24 zeros à esquerda
+		2'b10:	rd = {24'b0, Dataout[23:16]}; // Estende com 24 zeros à esquerda
+		2'b11:	rd = {24'b0, Dataout[31:24]}; // Estende com 24 zeros à esquerda
+	default:rd = 32'bX; // ou um valor de erro
+	endcase
 	end
         3'b001:	begin  //LH
 	 case (a[1])
-              	1'b0: rd <= {{16{Dataout[15]}}, Dataout[15:0]}; // Load half-word 0 (bits 15:0 of Dataout)
-                1'b1: rd <= {{16{Dataout[31]}}, Dataout[31:16]}; // Load half-word 1 (bits 31:16 of Dataout)
+              	1'b0: rd = {{16{Dataout[15]}}, Dataout[15:0]}; // Load half-word 0 (bits 15:0 of Dataout)
+                1'b1: rd = {{16{Dataout[31]}}, Dataout[31:16]}; // Load half-word 1 (bits 31:16 of Dataout)
                 default: rd = 'x; 
         endcase
 	end
         3'b010:	begin  //LW
-        rd <= Dataout;
+        rd = Dataout;
 	end
-        default: rd <= Dataout;
+        default: rd = Dataout;
       endcase
     end else if (MemWrite) begin
       case (Funct3)
       3'b000: begin // SB (Store Byte)
      	case (a[1:0])
-      	2'b00: begin Datain <= {24'b0, wd[7:0]}; Wr <= 4'b0001; end 
-      	2'b01: begin Datain <= {16'b0, wd[7:0], 8'b0}; Wr <= 4'b0010; end 
-      	2'b10: begin Datain <= {8'b0, wd[7:0], 16'b0}; Wr <= 4'b0100; end
-     	2'b11: begin Datain <= {wd[7:0], 24'b0}; Wr <= 4'b1000; end 
-      	default: Wr <= 4'b0000; 
+      	2'b00: begin Datain = {Dataout[31:8], wd[7:0]}; Wr = 4'b0001; end 
+      	2'b01: begin Datain = {Dataout[31:16], wd[7:0], Dataout[7:0]}; Wr = 4'b0010; end 
+      	2'b10: begin Datain = {Dataout[31:24], wd[7:0], Dataout[15:0]}; Wr = 4'b0100; end
+     	2'b11: begin Datain = {wd[7:0], Dataout[23:0]}; Wr = 4'b1000; end 
+      	default: Wr = 4'b0000; 
      	endcase
        end
        3'b001: begin // SH (Store Half-word)
          case (a[1])
-         1'b0: begin Datain <= {16'b0, wd[15:0]}; Wr <= 4'b0011; end 
-         1'b1: begin Datain <= {wd[15:0], 16'b0}; Wr <= 4'b1100; end 
-         default: Wr <= 4'b0000;
+         1'b0: begin Datain = {Dataout[31:16], wd[15:0]}; Wr = 4'b0011; end 
+         1'b1: begin Datain = {wd[15:0], Dataout[15:0]}; Wr = 4'b1100; end 
+         default: Wr = 4'b0000;
          endcase
         end
         3'b010: begin // SW (Store Word)
-          Wr <= 4'b1111; 
-          Datain <= wd;  
+          Wr = 4'b1111; 
+          Datain = wd;  
           end
           default: begin
-          Wr <= 4'b0000;
-          Datain <= 'x; 
+          Wr = 4'b0000;
+          Datain = 'x; 
           end
          endcase
         end
